f3587c9887788ac42806f4768b5fc7fa458adce9
Alson Shareef <alson.shareef@gmail.com>
2018-11-28 22:49:51 +0800

    Added round score functionality

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
 let scores, roundScore, activePlayer;
@@ -26,12 +25,34 @@ document.getElementById('current-1').textContent = '0';
 // Click event for roll button to display dice image depending on dice roll
 document.querySelector('.btn-roll').addEventListener('click', () => {
     let dice = Math.floor(Math.random() * 6) + 1;
+    
     let diceDom = document.querySelector('.dice');
-
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
+
+    // Roundscore functionality
+    if (dice !== 1) {
+        // Adding dice roll to current round score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Switch active player and reset roud score
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        // Display the reset round score
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+
+        // Toggle the active player styles
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        diceDom.style.display = 'none';
+    }
+
 })
 
 
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+
