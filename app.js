8dbad2fcfad1e200b0e4c55e7db6da8610cba50a
Alson Shareef <alson.shareef@gmail.com>
2018-11-29 20:26:51 +0800

    Added game initializer function

--- app.js
+++ app.js
@@ -9,26 +9,14 @@ GAME RULES:
 */
 
 let scores, roundScore, activePlayer, diceDom;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-diceDom = document.querySelector('.dice');
-
-// Hide dice
-document.querySelector('.dice').style.display = 'none';
-
-// Set all scores to 0
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 // Click event for roll button to display dice image depending on dice roll
     document.querySelector('.btn-roll').addEventListener('click', () => {
         let dice = Math.floor(Math.random() * 6) + 1;
         
-        diceDom.style.display = 'block';
-        diceDom.src = 'dice-' + dice + '.png';
+        document.querySelector('.dice').style.display = 'block';
+        document.querySelector('.dice').src = 'dice-' + dice + '.png';
 
         // Roundscore functionality
         if (dice !== 1) {
@@ -51,24 +39,55 @@ document.getElementById('current-1').textContent = '0';
             document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
-            diceDom.style.display = 'none';
+            document.querySelector('.dice').style.display = 'none';
         } else {
             nextPlayer(); 
         }
     });
 
-let nextPlayer = () => {
-    // Switch active player and reset round score
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
+// Next Player
+    const nextPlayer = () => {
+        // Switch active player and reset round score
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
-    // Display the reset round score
-    document.getElementById('current-0').textContent = 0;
-    document.getElementById('current-1').textContent = 0;
+        // Display the reset round score
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
 
-    // Toggle the active player styles
-    document.querySelector('.player-0-panel').classList.toggle('active')
-    document.querySelector('.player-1-panel').classList.toggle('active')
+        // Toggle the active player styles
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
 
-    diceDom.style.display = 'none';
-}
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+// Click event for new game button to reset game
+    document.querySelector('.btn-new').addEventListener('click', init);
+
+// Game Initializer
+    function init() {
+        scores = [0, 0];
+        roundScore = 0;
+        activePlayer = 0;
+
+        // Hide dice
+        document.querySelector('.dice').style.display = 'none';
+
+        // Set all scores to 0
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // Set all labels to player name
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
+
+        // Reset winner classes and player 0 to active
+        document.querySelector('.player-0-panel').classList.remove('winner')
+        document.querySelector('.player-1-panel').classList.remove('winner')
+        document.querySelector('.player-0-panel').classList.remove('active')
+        document.querySelector('.player-1-panel').classList.remove('active')
+        document.querySelector('.player-0-panel').classList.add('active')
+    }
